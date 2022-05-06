86b8f1a24b8437dcc630a30995916ebf4596b333
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-05-06 14:40:37 +0100

    Tailwing bg and color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./assets/img/melanie-weidmann.jpg')",
+      },
+    },
+    container: {
+      center: true,
+    },
+    colors: {
+      'c-blue': '#725BDA',
+    },
   },
   plugins: [],
 };

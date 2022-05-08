141d78d5a20ff69ae65e5b8ecb95635e1c9586d4
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-05-08 20:51:03 +0100

    Folder name changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import Signup from './pages/signup';
-import Login from './pages/Login';
-import Wallet from './pages/Wallet';
+import Signup from './Pages/signup';
+import Login from './Pages/Login';
+import Wallet from './Pages/Wallet';
 
 import './App.css';
-import Landing from './pages/Landing';
+import Landing from './Pages/Landing';
 
 function App() {
   return (

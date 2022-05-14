35f1ffae3ff34d3ea6f06b83148dd875f4ba00ff
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-05-14 12:11:58 +0100

    Explore page implementation in progress

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Wallet from './Pages/Wallet';
 
 import './App.css';
 import Landing from './Pages/Landing';
+import Explore from './Pages/Explore';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/explore" element={<Explore />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/wallet" element={<Wallet />} />
       </Routes>
+      {/* <Footer /> */}
     </div>
   );
 }

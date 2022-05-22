b546db0cd406716433f20e7f100ff727557e16be
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-05-22 14:09:42 +0100

    Changed Invalid DOM property `class`

--- src/components/Formbutton.jsx
+++ src/components/Formbutton.jsx
@@ -4,7 +4,7 @@ function Button({ buttonTitle }) {
   return (
     <button
       style={{ background: '#735BDC', color: '#fff' }}
-      class=" ... bg-white px-10 py-2 mx-auto mt-5 rounded-full text-white text-xl"
+      className=" ... bg-white px-10 py-2 mx-auto mt-5 rounded-full text-white text-xl"
     >
       {buttonTitle}
     </button>
